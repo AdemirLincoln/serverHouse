2b6d7ae68452f68a72c0c7e55ede1a0d9fa96b45
AdemirLincoln <desenvolvimento2@7cliques.com>
2015-10-29 08:05:05 -0200

    axxa

--- global.php
+++ global.php
@@ -1,6 +1,6 @@
 <?php 
 
-	$con = new PDO ( "mysql:host=localhost;dbname=serverhouse", "root", "root" );
+	$con = new PDO ( "mysql:host=localhost;dbname=serverho_serverhouse2015", "serverho_ademir", "work#007" ); 
 
     if (!$con) {
         echo "Erro de Conexao";
